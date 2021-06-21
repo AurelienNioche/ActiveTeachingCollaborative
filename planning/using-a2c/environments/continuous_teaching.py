15f5950c1423db04e207213c7d7db2974e4e39e6
sina <sina.rismanchian@gmail.com>
2021-06-21 20:05:48 +0430

    change of continuous_teaching.py env for human like agents.

--- planning/using-a2c/environments/continuous_teaching.py
+++ planning/using-a2c/environments/continuous_teaching.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class ContinuousTeaching(gym.Env, ABC):
 
-    def __init__(self, alpha=1., beta=0.2, tau=0.9, n_item=30, t_max=1000,
+    def __init__(self, initial_forget_rates: np.ndarray, initial_repetition_rates: np.ndarray, tau=0.9, n_item=30, t_max=1000,
                  time_per_iter=1):
         super().__init__()
 
@@ -19,8 +19,11 @@ class ContinuousTeaching(gym.Env, ABC):
         self.t_max = t_max
         self.time_per_iter = time_per_iter
         self.log_tau = np.log(tau)
-        self.alpha = alpha
-        self.beta = beta
+
+        if initial_repetition_rates.shape[0] == n_item and \
+                initial_forget_rates.shape[0] == n_item:
+            self.initial_forget_rates = initial_forget_rates
+            self.initial_repetition_rates = initial_repetition_rates
 
         self.t = 0
 
@@ -42,15 +45,16 @@ class ContinuousTeaching(gym.Env, ABC):
         delta = self.state[view, 0]       # only consider already seen items
         rep = self.state[view, 1] - 1.    # only consider already seen items
 
-        forget_rate = self.alpha * (1 - self.beta) ** rep
+        forget_rate = self.initial_forget_rates[view] * \
+                      (1 - self.initial_repetition_rates[view]) ** rep
 
         logp_recall = - forget_rate * delta
         above_thr = logp_recall > self.log_tau
         reward = np.count_nonzero(above_thr) / self.n_item
-
+        # print(self.obs[view, 0])
         # Probability of recall at the time of the next action
-        self.obs[view, 0] = self.obs[view, 0] = \
-            np.exp(-forget_rate * (delta + self.time_per_iter))
+        self.obs[view, 0] = np.exp(-forget_rate * (delta + self.time_per_iter))
+
         # Forgetting rate of probability of recall
         self.obs[view, 1] = forget_rate
 
