ec2db3e08f99a509c51eb24e9ad2d28bde163761
Aurélien Nioche <aureliennioche@Aureliens-MacBook-Pro.local>
2022-02-25 23:45:44 +0000

    update

--- inference/train.py
+++ inference/train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from torch import optim
-from tqdm.autonotebook import tqdm
+from tqdm import tqdm
 
 from . flows import NormalizingFlows
 from . loss import LossTeaching
